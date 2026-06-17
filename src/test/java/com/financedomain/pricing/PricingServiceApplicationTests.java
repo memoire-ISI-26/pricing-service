37ec6551b36b703b525359d1ef117d17bd2a8d5b
Ibrahim-svmb <ibrahim.svmb@gmail.com>
2026-06-17 21:45:49 +0000

    Initial commit

---
+++ src/test/java/com/financedomain/pricing/PricingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.financedomain.pricing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PricingServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
