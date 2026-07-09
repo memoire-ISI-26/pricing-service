096d2e45da5c28b95f5c0c2e259bf9b8101dea9a
Ibrahim-svmb <ibrahim.svmb@gmail.com>
2026-07-09 15:42:37 +0000

    feat: set up the tracking proxy

---
+++ src/main/java/com/financedomain/pricing/proxy/TrackingProxy.java
@@ -0,0 +1,18 @@
+package com.financedomain.pricing.proxy;
+
+import com.financedomain.pricing.dto.TrackingEvent;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
+
+@FeignClient(name = "tracking-service")
+public interface TrackingProxy {
+
+    @PostMapping("/tracking/event")
+    ResponseEntity<?> collectEvent(
+            @RequestBody TrackingEvent event,
+            @RequestHeader("X-User-Role") String xUserRole
+    );
+}
