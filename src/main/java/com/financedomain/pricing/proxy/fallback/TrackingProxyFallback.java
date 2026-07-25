46a556a9851e1bac5dba3383bba7026e52dd995a
Ibrahim-svmb <ibrahim.svmb@gmail.com>
2026-07-25 18:18:33 +0000

    fix: clean code

--- src/main/java/com/financedomain/pricing/proxy/fallback/TrackingProxyFallback.java
+++ src/main/java/com/financedomain/pricing/proxy/fallback/TrackingProxyFallback.java
@@ -11,7 +11,7 @@ public class TrackingProxyFallback implements TrackingProxy {
     @Override
     public ResponseEntity<?> collectEvent(TrackingEvent event, String xUserRole) {
         System.err.println("[Fallback] tracking-service est indisponible. Événement de tracking ignoré : " + event.getEventType());
-        // Retourne un succès fictif pour ne pas bloquer les achats de pass ou crédit
+
         return ResponseEntity.ok().build();
     }
 }
